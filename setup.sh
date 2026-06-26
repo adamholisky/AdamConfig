903d5f87e3bd5ac9ea17b825e7d546c99202e221
Adam Holisky <adam.holisky@gmail.com>
2026-06-25 22:26:41 -0500

    updates

--- setup.sh
+++ setup.sh
@@ -1,8 +1,45 @@
 #!/bin/bash
 
-cd /home/adam
+#
+# Defines
+#
+HOME_DIR=/home/adam
+CONFIG_DIR=/home/adam/AdamConfig
 
-ln -s /home/adam/AdamConfig/alacritty/ .config/alacritty
-ln -s /home/adam/AdamConfig/zshrc .zshrc
-ln -s /home/adam/AdamConfig/tmux.conf .tmux.conf
-ln -s /home/adam/AdamConfig/tmuxp/ tmuxp
+#
+# Command Processing
+#
+CMD=${1:all}
+
+do_setup_tmux() {
+	ln -s $CONFIG_DIR/tmux/tmux.conf $HOME_DIR/.tmux.conf
+	ln -s $CONFIG_DIR/tmuxp/ $HOME_DIR/tmuxp
+}
+
+do_setup_alacritty() {
+	ln -s $CONFIG_DIR/alacritty/ $HOME_DIR/.config/alacritty
+}
+
+do_setup_zsh() {
+	$CONFIG_DIR/zsh/setup-zsh-config.sh
+}
+
+case "$CMD" in
+	all)
+		do_setup_alacritty
+		;;
+	alacritty)
+		do_setup_alacritty
+		;;
+	tmux)
+		do_setup_tmux
+		;;
+	zsh)
+		do_setup_zsh
+		;;
+	*)
+		echo "Usage: setup.sh {all|alacritty|tmux|zsh}"
+		exit 1
+		;;
+esac
+
---
+++ zsh/setup-zsh-config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#
+# Defines
+#
+HOME_DIR=/home/adam
+CONFIG_DIR=/home/adam/AdamConfig
+
+ln -s $CONFIG_DIR/zsh/config/zshrc $HOME_DIR/.zshrc
