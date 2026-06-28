93ef29e1480e302dac9487d1ef1a5a0fb4e1590d
Adam Holisky <adam.holisky@gmail.com>
2026-06-27 22:11:50 -0500

    various updates

---
+++ nvim/lua/config/set.lua
@@ -0,0 +1,14 @@
+vim.opt.clipboard = 'unnamedplus' 	-- use sys kb for yank
+
+vim.opt.nu = true 			-- line numbers
+vim.opt.relativenumber = false		-- relative line nums
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = false
+vim.opt.smartindent = true
+
+vim.opt.wrap = true
+
+vim.opt.termguicolors = true
